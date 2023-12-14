0ae1f2670791ddc6f5eb38b375ec8706459eec32
Chidiebere Macey <egbomaceyskyla@gmail.com>
2023-12-14 08:54:25 +0100

    made changes to js file

---
+++ js/main.js
@@ -0,0 +1,26 @@
+class User{
+    constructor(userName,lastName){
+        this.name = userName,
+        this.lastname = lastName
+    }
+    login(){
+        console.log(`${this.name} just logged in`)
+    }
+    logout(){
+        console.log(`${this.lastname} just logged out`)
+    }
+}
+
+class Admin extends User{
+    deleteUser(userdel){
+        console.log(`${this.name} deleted user ${userdel}`)
+    }
+}
+
+let userOne = new User('Chinaza', 'Edeh')
+let userTwo = new User('Chidubem', 'Wisdom')
+let adminOne = new Admin('paul', 'patrick')
+
+adminOne.deleteUser(userOne.lastname)
+userOne.logout()
+userTwo.login()
